05d164e557c1d06bbaca95b32671b5d60cc1ba60
try1233 <108941598+try1233@users.noreply.github.com>
2023-11-11 15:08:28 +0100

    Update normalize_features.py

--- preprocessing/normalize_features.py
+++ preprocessing/normalize_features.py
@@ -1,1 +1,14 @@
+from sklearn.base import BaseEstimator, TransformerMixin
 
+
+class NormalizeFeatures(BaseEstimator, TransformerMixin):
+    "Reduce Size of DataFrame."
+
+    def __init__(self):
+        pass
+
+    def fit(self, df, y=None):
+        return self
+
+    def transform(self, df):
+        return df
