9c3f48ef4fed35091aaa8e6206b914844c30f17d
try1233 <108941598+try1233@users.noreply.github.com>
2023-11-11 14:09:26 +0100

    Update drop_features.py
    Nan target values drop

--- preprocessing/drop_features.py
+++ preprocessing/drop_features.py
@@ -1,5 +1,18 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
+class DropNans(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, df, y=None):
+        return self
+
+    def transform(self, df):
+        df = df.dropna(subset=['imbalance_size', 'bid_price', 'ask_price', 'wap', 'target'])
+        feature_name = ['imbalance_size', 'bid_price', 'ask_price', 'wap', 'target']
+        df = df[feature_name]
+        return df
+     
 
 class DropFeatures(BaseEstimator, TransformerMixin):
     def __init__(self):
