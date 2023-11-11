dbcc3071f4988218f5cacd4391437073c8cf0fc1
try1233 <108941598+try1233@users.noreply.github.com>
2023-11-11 22:31:12 +0100

    Create add_gaussian.py

---
+++ preprocessing/add_gaussian.py
@@ -0,0 +1,18 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class AddGaussianNoise(BaseEstimator, TransformerMixin):
+
+    def __init__(self, scale = 4):
+      self.scale = scale
+        
+    def fit(self, df, y=None):
+        self.df_noise_scale = df.std(numeric_only = True) ** 1/self.scale
+        return self
+
+    def transform(self, df):
+        non_numeric_features = ["stock_id","date_id", "seconds_in_bucket","time_id", "row_id"] 
+        for column in df.columns:
+          if column not in non_numeric_features:
+            df[column] += np.random.normal(0,df_noise_scale[column], [df.shape[0],])
+        return df
