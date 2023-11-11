46ca33fda89d83fd436359befde8512d7e595ebf
try1233 <108941598+try1233@users.noreply.github.com>
2023-11-11 16:12:02 +0100

    Update fill_nan_features.py

--- preprocessing/fill_nan_features.py
+++ preprocessing/fill_nan_features.py
@@ -4,22 +4,21 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class FillNanFeatures(BaseEstimator, TransformerMixin):
     "Reduce Size of DataFrame."
 
-    def __init__(self):
-        pass
-
-    def fit(self, df, y=None, method = "median"):
+    def __init__(self, method = "median"):
+        self.method = method
         if method not in ["median", "mean", "zeros"]:
             raise ValueError("""method not found, has to be one of "median", "mean", "zeros" """)
-        return self
+
+    def fit(self, df, y=None):
         self.features = [i for i in df.columns if i not in ["row_id", "target", "time_id", "date_id"]]
-        if method == "median":
+        if self.method == "median":
             self.fillvalues = df[self.features].median(skipna=True)
-        elif method == "mean":
+        elif self.method == "mean":
             self.fillvalues = df[self.features].mean(skipna=True)
-        elif method == "zeros":
+        elif self.method == "zeros":
             self.fillvalues = 0.0
+        return self
         
     def transform(self, df):
-        features = [i for i in df.columns if i not in ["row_id", "target", "time_id", "date_id"]]
-        df = df[self.features].fillna(self.fillvalues)
+        df[self.features] = df[self.features].fillna(self.fillvalues)
         return df
