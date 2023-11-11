b885ca00034a5d112b14e2a05bf851f5a63554db
nifleisch <62500421+nifleisch@users.noreply.github.com>
2023-11-11 02:43:18 +0100

    Remove WandB logging from eight_fold_cv function

--- evaluation/eight_fold_cv.py
+++ evaluation/eight_fold_cv.py
@@ -1,20 +1,13 @@
 from sklearn.model_selection import GroupKFold
 from sklearn.metrics import mean_absolute_error
 import numpy as np
-import wandb
 
 
 def eight_fold_cv(model_class, params, preprocessor, df):
-    wandb.init(project='optiver_trading')
-    wandb.config.eval_strategy = '8-fold'
-    wandb.config.preprocessor = list(preprocessor.named_steps.keys())
-    wandb.config.model_class = model_class.__name__
-    wandb.config.params = params
     X = df.copy()
     y = X.pop('target')
     mae_scores = []
     gfk = GroupKFold(n_splits=8)
-    fold = 1
     for train_index, test_index in gfk.split(X, y, groups=df['date_id']):
         X_train, X_test = X.iloc[train_index].copy(), X.iloc[test_index].copy()
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -28,12 +21,7 @@ def eight_fold_cv(model_class, params, preprocessor, df):
         y_pred = model.predict(X_test_processed)
         mae = mean_absolute_error(y_test, y_pred)
         mae_scores.append(mae)
-        wandb.log({f'fold_{fold}_mae': mae})
-        fold += 1
 
     mean_mae = np.mean(mae_scores)
     std_mae = np.std(mae_scores)
-
-    wandb.log({'mean_mae': mean_mae, 'std_mae': std_mae})
-    wandb.finish()
     return mean_mae, std_mae
