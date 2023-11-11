8686ddc2e8ad1bc1352f335ac324e81cafaf160e
nifleisch <62500421+nifleisch@users.noreply.github.com>
2023-11-11 02:40:01 +0100

    Fix copy error in eight_fold_cv.py

--- evaluation/eight_fold_cv.py
+++ evaluation/eight_fold_cv.py
@@ -16,7 +16,7 @@ def eight_fold_cv(model_class, params, preprocessor, df):
     gfk = GroupKFold(n_splits=8)
     fold = 1
     for train_index, test_index in gfk.split(X, y, groups=df['date_id']):
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        X_train, X_test = X.iloc[train_index].copy(), X.iloc[test_index].copy()
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
         X_train_processed = preprocessor.fit_transform(X_train)
