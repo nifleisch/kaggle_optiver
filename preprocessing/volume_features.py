f471c91c832e5262b0bb65a43af2eb69b37643b5
try1233 <lennard.schaub@gmail.com>
2023-12-13 16:18:44 +0100

    Add 'imbalance_change' feature to VolumeFeatures

--- preprocessing/volume_features.py
+++ preprocessing/volume_features.py
@@ -14,6 +14,7 @@ class VolumeFeatures(BaseEstimator, TransformerMixin):
     def transform(self, df):
         df["volume"] = df.eval("ask_size + bid_size")
         df["depth_pressure"] = (df["ask_size"] - df["bid_size"]) * (df["far_price"] - df["near_price"]) #Ist bereits in price_features
+        df['imbalance_change'] = df["imbalance_size"] - df.groupby('stock_id')['imbalance_size'].shift(1)
         df["imbalance"] = df["imbalance_size"] * df["imbalance_buy_sell_flag"]
         df["liquidity_imbalance"] = df.eval("(bid_size-ask_size)/(bid_size+ask_size)")
         df["matched_imbalance"] = df.eval("(imbalance_size-matched_size)/(matched_size+imbalance_size)")
