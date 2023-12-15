6cb3a104c08b67d5bbcdab5d4ae6742ee2032b92
try1233 <lennard.schaub@gmail.com>
2023-12-15 22:15:02 +0100

    minor changes

---
+++ preprocessing/imbalance_features.py
@@ -0,0 +1,112 @@
+import warnings
+from warnings import simplefilter
+from itertools import combinations
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+import gc 
+import pandas as pd
+
+
+warnings.filterwarnings("ignore")
+simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+
+class ImbalanceFeatures(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        pass
+
+    def fit(self, df, y=None):
+        return self
+    
+    def transform(self, df):
+        df = imbalance_features(df)
+        return df
+
+
+
+def imbalance_features(df):
+    # Define lists of price and size-related column names
+    prices = ["reference_price", "far_price", "near_price", "ask_price", "bid_price", "wap"]
+    sizes = ["matched_size", "bid_size", "ask_size", "imbalance_size"]
+    
+   
+  
+    df["liquidity_imbalance"] = df.eval("(bid_size-ask_size)/(bid_size+ask_size)")
+    df["matched_imbalance"] = df.eval("(imbalance_size-matched_size)/(matched_size+imbalance_size)")
+
+    for c in combinations(prices, 2):
+        df[f"{c[0]}_{c[1]}_imb"] = df.eval(f"({c[0]} - {c[1]})/({c[0]} + {c[1]})")
+
+   
+    
+    df["stock_weights"] = df["stock_id"].map(weights)
+    df["weighted_wap"] = df["stock_weights"] * df["wap"]
+    df['wap_momentum'] = df.groupby('stock_id')['weighted_wap'].pct_change(periods=6)
+   
+    df["imbalance_momentum"] = df.groupby(['stock_id'])['imbalance_size'].diff(periods=1) / df['matched_size']
+    df["price_spread"] = df["ask_price"] - df["bid_price"]
+    df["spread_intensity"] = df.groupby(['stock_id'])['price_spread'].diff()
+    df['price_pressure'] = df['imbalance_size'] * (df['ask_price'] - df['bid_price'])
+    df['market_urgency'] = df['price_spread'] * df['liquidity_imbalance']
+    df['depth_pressure'] = (df['ask_size'] - df['bid_size']) * (df['far_price'] - df['near_price'])
+    
+    df['spread_depth_ratio'] = (df['ask_price'] - df['bid_price']) / (df['bid_size'] + df['ask_size'])
+    df['mid_price_movement'] = df['mid_price'].diff(periods=5).apply(lambda x: 1 if x > 0 else (-1 if x < 0 else 0))
+    
+    df['micro_price'] = ((df['bid_price'] * df['ask_size']) + (df['ask_price'] * df['bid_size'])) / (df['bid_size'] + df['ask_size'])
+    df['relative_spread'] = (df['ask_price'] - df['bid_price']) / df['wap']
+    
+    # Calculate various statistical aggregation features
+    for func in ["mean", "std", "skew", "kurt"]:
+        df[f"all_prices_{func}"] = df[prices].agg(func, axis=1)
+        df[f"all_sizes_{func}"] = df[sizes].agg(func, axis=1)
+        
+
+    for col in ['matched_size', 'imbalance_size', 'reference_price', 'imbalance_buy_sell_flag']:
+        for window in [1,3,5,10]:
+            df[f"{col}_shift_{window}"] = df.groupby('stock_id')[col].shift(window)
+            df[f"{col}_ret_{window}"] = df.groupby('stock_id')[col].pct_change(window)
+    
+    # Calculate diff features for specific columns
+    for col in ['ask_price', 'bid_price', 'ask_size', 'bid_size', 'weighted_wap','price_spread']:
+        for window in [1,3,5,10]:
+            df[f"{col}_diff_{window}"] = df.groupby("stock_id")[col].diff(window)
+    
+    #V4 feature
+    for window in [3,5,10]:
+        df[f'price_change_diff_{window}'] = df[f'bid_price_diff_{window}'] - df[f'ask_price_diff_{window}']
+        df[f'size_change_diff_{window}'] = df[f'bid_size_diff_{window}'] - df[f'ask_size_diff_{window}']
+
+  
+    gc.collect()
+    
+    df['mid_price*volume'] = df['mid_price_movement'] * df['volume']
+    df['harmonic_imbalance'] = df.eval('2 / ((1 / bid_size) + (1 / ask_size))')
+    
+    for col in df.columns:
+        df[col] = df[col].replace([np.inf, -np.inf], 0)
+
+    return df
+
+
+
+weights = [
+    0.004, 0.001, 0.002, 0.006, 0.004, 0.004, 0.002, 0.006, 0.006, 0.002, 0.002, 0.008,
+    0.006, 0.002, 0.008, 0.006, 0.002, 0.006, 0.004, 0.002, 0.004, 0.001, 0.006, 0.004,
+    0.002, 0.002, 0.004, 0.002, 0.004, 0.004, 0.001, 0.001, 0.002, 0.002, 0.006, 0.004,
+    0.004, 0.004, 0.006, 0.002, 0.002, 0.04 , 0.002, 0.002, 0.004, 0.04 , 0.002, 0.001,
+    0.006, 0.004, 0.004, 0.006, 0.001, 0.004, 0.004, 0.002, 0.006, 0.004, 0.006, 0.004,
+    0.006, 0.004, 0.002, 0.001, 0.002, 0.004, 0.002, 0.008, 0.004, 0.004, 0.002, 0.004,
+    0.006, 0.002, 0.004, 0.004, 0.002, 0.004, 0.004, 0.004, 0.001, 0.002, 0.002, 0.008,
+    0.02 , 0.004, 0.006, 0.002, 0.02 , 0.002, 0.002, 0.006, 0.004, 0.002, 0.001, 0.02,
+    0.006, 0.001, 0.002, 0.004, 0.001, 0.002, 0.006, 0.006, 0.004, 0.006, 0.001, 0.002,
+    0.004, 0.006, 0.006, 0.001, 0.04 , 0.006, 0.002, 0.004, 0.002, 0.002, 0.006, 0.002,
+    0.002, 0.004, 0.006, 0.006, 0.002, 0.002, 0.008, 0.006, 0.004, 0.002, 0.006, 0.002,
+    0.004, 0.006, 0.002, 0.004, 0.001, 0.004, 0.002, 0.004, 0.008, 0.006, 0.008, 0.002,
+    0.004, 0.002, 0.001, 0.004, 0.004, 0.004, 0.006, 0.008, 0.004, 0.001, 0.001, 0.002,
+    0.006, 0.004, 0.001, 0.002, 0.006, 0.004, 0.006, 0.008, 0.002, 0.002, 0.004, 0.002,
+    0.04 , 0.002, 0.002, 0.004, 0.002, 0.002, 0.006, 0.02 , 0.004, 0.002, 0.006, 0.02,
+    0.001, 0.002, 0.006, 0.004, 0.006, 0.004, 0.004, 0.004, 0.004, 0.002, 0.004, 0.04,
+    0.002, 0.008, 0.002, 0.004, 0.001, 0.004, 0.006, 0.004,
+]
+weights = {int(k):v for k,v in enumerate(weights)}
