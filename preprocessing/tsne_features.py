d2d8986db13d19f982145f60004e77e473f618f7
try1233 <lennard.schaub@gmail.com>
2023-12-17 02:51:51 +0100

    Refactor TSNEFeatures to handle missing target column

--- preprocessing/tsne_features.py
+++ preprocessing/tsne_features.py
@@ -12,16 +12,22 @@ class TSNEFeatures(BaseEstimator, TransformerMixin):
         
     def fit(self, df, y=None):
         self.tsne = TSNE(n_components=self.n)
-        self.tsne_trained = self.tsne.fit(df.drop(columns=['target']))
+        if 'target' in df.columns:
+            X = df.drop(columns=['target']).reset_index()
+        else:
+            X = df.reset_index()
+        self.tsne_trained = self.tsne.fit(X.groupby('stock_id').median().reset_index())
 
         return self
 
     def transform(self, df):
-        embedded_data = self.tsne_trained.transform(df)
+        if 'target' in df.columns:
+            X = df.drop(columns=['target']).reset_index()
+        else:
+            X = df.reset_index()
+        embedded_data = self.tsne_trained.transform(X.groupby('stock_id').median().reset_index())
         vector_df = pd.DataFrame(embedded_data)
         vector_df['stock_id'] = vector_df.index
         # Merge the original DataFrame with the vector DataFrame based on stock_id
         result_df = pd.merge(df, vector_df, on='stock_id', how='left')
         return result_df
-
-
