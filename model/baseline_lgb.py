d537c7c39f3665ee14d9ba7aeddb8cbc1dfbcc12
try1233 <lennard.schaub@gmail.com>
2023-12-18 21:45:57 +0100

    Add feature names to fit method in BaselineLGB class

--- model/baseline_lgb.py
+++ model/baseline_lgb.py
@@ -1,7 +1,7 @@
 from .optiver_model import OptiverModel
 import pandas as pd
 import lightgbm as lgb
-
+import numpy as np
 
 class BaselineLGB(OptiverModel):
     def __init__(self, params: dict):
@@ -9,7 +9,9 @@ class BaselineLGB(OptiverModel):
 
     def fit(self, X: pd.DataFrame, y: pd.Series, X_val: pd.DataFrame = None, y_val: pd.Series = None):
         if X_val is not None:
-            self.model.fit(X, y, eval_set=[(X_val, y_val)])
+            feature_names = X.columns.tolist()
+            self.model.fit(X, y, eval_set=[(X_val, y_val)], feature_name=feature_names)
+            #self.model.fit(X, y, eval_set=[(X_val, y_val)])
         else:
             self.model.fit(X, y)
 
